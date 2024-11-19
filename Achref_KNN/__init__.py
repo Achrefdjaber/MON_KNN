9ed034fe9df36124f7bcd63cdb08bb563b20cbb9
DJABER-Achref <123998407+Achrefdjaber@users.noreply.github.com>
2024-11-19 09:17:01 +0100

    Update __init__.py

--- Achref_KNN/__init__.py
+++ Achref_KNN/__init__.py
@@ -1,18 +1,1 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='Achref_KNN',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=['numpy'],
-    description='Ma propre implémentation du classificateur KNN',
-    author='Ton Nom',
-    author_email='ton.email@example.com',
-    url='https://github.com/Achrefdjaber/Achref_KNN',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+from .KNN import KNN
